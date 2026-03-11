7ab7ac77cce56130dfc97c566d7648f6a55e0853
tierlist <gregoriomontoya608@gmail.com>
2026-03-11 00:25:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,116 @@
-function mostrarMensaje(){
+const ADMIN_PASSWORD = "AuroraAdmin123"
 
-alert("Bienvenido a AuroraTI 🚀");
+let players = JSON.parse(localStorage.getItem("aurora_players")) || []
+
+function login(){
+
+const pass=document.getElementById("adminPass").value
+
+if(pass===ADMIN_PASSWORD){
+
+document.getElementById("adminControls").classList.remove("hidden")
+document.getElementById("loginPanel").style.display="none"
+
+}else{
+
+alert("Contraseña incorrecta")
 
 }
+
+}
+
+function save(){
+localStorage.setItem("aurora_players",JSON.stringify(players))
+}
+
+function getIcon(mode){
+
+if(mode==="Mazo") return "⚒"
+if(mode==="Diapot") return "🧪"
+if(mode==="Sword") return "⚔"
+if(mode==="CPvP") return "🛡"
+if(mode==="Nethepot") return "🔥"
+if(mode==="UHC") return "❤"
+if(mode==="SMP") return "🌍"
+
+}
+
+function render(){
+
+const container=document.getElementById("players")
+container.innerHTML=""
+
+players.forEach((p,index)=>{
+
+let tiersHTML=""
+
+p.tiers.forEach(t=>{
+
+tiersHTML+=`<div class="tier">${t.mode} ${t.tier}</div>`
+
+})
+
+container.innerHTML+=`
+
+<div class="player">
+
+<div class="left">
+
+<img class="skin" src="https://mc-heads.net/avatar/${p.nick}">
+
+<div class="name">${p.nick}</div>
+
+</div>
+
+<div class="modes">
+${tiersHTML}
+</div>
+
+<button class="delete" onclick="removePlayer(${index})">Eliminar</button>
+
+</div>
+
+`
+
+})
+
+}
+
+function addTier(){
+
+const nick=document.getElementById("nick").value
+const tier=document.getElementById("tier").value
+const mode=document.getElementById("mode").value
+
+if(nick==="") return
+
+let player=players.find(p=>p.nick.toLowerCase()===nick.toLowerCase())
+
+if(!player){
+
+player={
+nick:nick,
+tiers:[]
+}
+
+players.push(player)
+
+}
+
+player.tiers.push({mode,tier})
+
+save()
+render()
+
+}
+
+function removePlayer(i){
+
+players.splice(i,1)
+
+save()
+render()
+
+}
+
+render()
